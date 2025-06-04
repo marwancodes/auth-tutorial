6f808ad18f121329ab90fe68bbe06e6b22ca7d49
marwancoder <marwane.ouarradi@gmail.com>
2025-06-04 15:20:57 +0100

    Database Connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,4 +15,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB();
 });
