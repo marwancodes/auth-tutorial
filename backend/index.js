721272d967c3c6db27503d5c1af5d8a124d72876
marwancoder <marwane.ouarradi@gmail.com>
2025-06-04 15:07:49 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World 12333!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
