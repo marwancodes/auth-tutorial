8593e4a9f451b3fd128629ab1a521b3cef7a5477
marwancoder <marwane.ouarradi@gmail.com>
2025-06-13 15:45:16 +0100

    URL updated in authStore.js

--- frontend/src/store/authStore.jsx
+++ frontend/src/store/authStore.jsx
@@ -2,8 +2,8 @@ import { create } from "zustand";
 import axios from "axios";
 
 
-// const API_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api/auth" : "/api/auth";
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api/auth" : "/api/auth";
+// const API_URL = "http://localhost:3000/api/auth";
 
 axios.defaults.withCredentials = true;
 
